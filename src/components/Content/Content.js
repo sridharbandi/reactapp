2446b20f1f2af5006f8d791efd06de90a489a8f4
sridharbandi <sridhar.bandi.ece@gmail.com>
2018-07-17 20:21:31 +0100

    Added button and styles

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 import {withStyles} from '@material-ui/core/styles';
 import Aux from "../../hoc/Aux";
 
 
 const styles = theme => ({
-    textField: {
+    textColor: {
         color: 'white',
     }
 });
@@ -14,11 +16,15 @@ const styles = theme => ({
 class Content extends Component {
     state = {
         text: '',
+        textentered: ''
     };
 
     textChangeHandler = (event) => {
         this.setState({text: event.target.value});
     };
+
+
+
 
     render() {
         return (
@@ -28,7 +34,7 @@ class Content extends Component {
                         <form noValidate autoComplete="off">
                             <TextField
                                 InputProps={{
-                                    className: this.props.classes.textField
+                                    className: this.props.classes.textColor
                                 }}
                                 id="idtext"
                                 label="Text"
@@ -40,7 +46,18 @@ class Content extends Component {
                                 fullWidth
                                 error
                             />
+                            <Button
+                                id="idbutton"
+                                variant="raised"
+                                fullWidth
+                                color="secondary">
+                                Get Text
+                            </Button>
                         </form>
+                        <br />
+                        <Typography variant="body1" component="p" className={this.props.classes.textColor}>
+                            Text Entered: {this.state.textentered}
+                        </Typography>
                     </Grid>
                 </Grid>
             </Aux>
